95f94685bccefac4e2f3f6daf35b6265b15a2b50
Luca Giorgetti <lucagiorgit95@gmail.com>
2020-06-26 11:59:35 +0200

    Pulizia unused

--- app/src/main/java/com/lucagiorgetti/surprix/ui/mainfragments/setdetail/SetDetailFragment.java
+++ app/src/main/java/com/lucagiorgetti/surprix/ui/mainfragments/setdetail/SetDetailFragment.java
@@ -8,7 +8,6 @@ import android.view.ViewGroup;
 import android.widget.ProgressBar;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
